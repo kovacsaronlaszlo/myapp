4ad7aaa931ed6d262923e104f11794d67b77f2bc
karon <kovacs.aron@dlg.hu>
2018-01-22 19:36:29 +0100

    smart eventcard + use @Input decorator

---
+++ src/app/shared/event-model.ts
@@ -0,0 +1,12 @@
+export class EventModel {
+  id: number;
+  name: string;
+  date: string;
+  pictureURL: string;
+  description: string;
+
+  constructor(param?: EventModel) {
+    if (param)
+      Object.assign(this, param);
+  }
+}
