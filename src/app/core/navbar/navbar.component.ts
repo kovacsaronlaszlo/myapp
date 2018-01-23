085815bf610f75baf5b482f1037412a7e58c947c
karon <kovacs.aron@dlg.hu>
2018-01-23 17:51:41 +0100

    ng-container + if isLoggedin hide login and registration buttons in menu

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import {UserService} from "../../shared/user.service";
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  isCollapsed: boolean = true;
+  public isCollapsed = true;
 
   constructor(public userService: UserService) { }
 
