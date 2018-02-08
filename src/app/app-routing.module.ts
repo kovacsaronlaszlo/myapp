bb3ad85854aecd5e57534e8b8a475cdbb21b8f08
karon <kovacs.aron@dlg.hu>
2018-02-08 17:10:48 +0100

    [remove] = LoggedInGuard in app-routing module for developing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -41,7 +41,7 @@ const routes: Routes = [
     children: [
       {path: '', component: TicketListComponent},
       {path: 'new', component: TicketDetailComponent, canActivate: [LoggedInGuard]},
-      {path: ':id', component: BidComponent, canActivate: [LoggedInGuard]}
+      {path: ':id', component: BidComponent}
     ]
   },
   {path: 'about', component: AboutComponent},
