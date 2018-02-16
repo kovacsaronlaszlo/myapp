6651b39b1568d8dda328868a964017143ce57bdc
karon <kovacs.aron@dlg.hu>
2018-02-16 15:33:20 +0100

    [new] = refactoring bidWithBidStep output (move to bid form component)

--- src/app/ticket/bid-form/bid-form.component.ts
+++ src/app/ticket/bid-form/bid-form.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {TicketModel} from "../../shared/ticket-model";
 
 @Component({
   selector: 'app-bid-form',
   templateUrl: './bid-form.component.html',
   styleUrls: ['./bid-form.component.css']
 })
-export class BidFormComponent implements OnInit {
+export class BidFormComponent {
+  @Input() ticket: TicketModel;
+  @Output() bidWithBidStep = new EventEmitter<void>();
 
-  constructor() { }
-
-  ngOnInit() {
+  onBidWithBidStep() {
+    this.bidWithBidStep.emit();
   }
-
 }
--- src/app/ticket/bid/bid.component.ts
+++ src/app/ticket/bid/bid.component.ts
@@ -20,8 +20,4 @@ export class BidComponent implements OnInit {
     );
   }
 
-  onBidWithBidStep() {
-    alert('alert!');
-  }
-
 }
