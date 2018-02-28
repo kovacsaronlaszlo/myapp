a74e0210ed4812b0bf8d7305425a51bf1d7612cd
karon <k.aron.laszlo@gmail.com>
2018-02-28 21:17:59 +0100

    [modify] = bid component handle404

--- src/app/ticket/bid/bid.component.ts
+++ src/app/ticket/bid/bid.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {TicketService} from "../../shared/ticket.service";
 import {TicketModel} from "../../shared/ticket-model";
 import {UserService} from "../../shared/user.service";
-import {ActivatedRoute, ParamMap} from "@angular/router";
+import {ActivatedRoute, ParamMap, Router} from "@angular/router";
 
 @Component({
   selector: 'app-bid',
@@ -13,18 +13,31 @@ export class BidComponent implements OnInit {
   ticket: TicketModel;
   isLoggedIn: boolean;
 
-  constructor(
-    private ticketService: TicketService,
-    userService: UserService,
-    private route: ActivatedRoute) {
-      this.isLoggedIn = userService.isLoggedin;
+  constructor(private ticketService: TicketService,
+              userService: UserService,
+              private route: ActivatedRoute,
+              private router: Router) {
+    this.isLoggedIn = userService.isLoggedin;
   }
 
   ngOnInit() {
+    const handle404 = () => {
+      this.router.navigate(['404']);
+    };
+
     this.route.paramMap.subscribe(
       (params: ParamMap) => {
         this.ticketService.getOne(params.get('id')).subscribe(
-          ticket => this.ticket = ticket
+          ticket => {
+            if (ticket === null) {
+              handle404();
+            } else {
+              this.ticket = ticket;
+            }
+          },
+          err => {
+            return handle404();
+          }
         );
       }
     );
