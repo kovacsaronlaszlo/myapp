b49094a6c01a16c95f119391d069c0764e626a99
karon <kovacs.aron@dlg.hu>
2018-04-06 10:11:06 +0200

    [create] = core ngModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,43 +4,32 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {AlertModule, CollapseModule} from "ngx-bootstrap";
-import {NavbarComponent} from './core/navbar/navbar.component';
-import {JumbotronComponent} from './core/jumbotron/jumbotron.component';
-import {FooterComponent} from './core/footer/footer.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {EventService} from "./shared/event.service";
 import {UserService} from "./shared/user.service";
 import {TicketService} from "./shared/ticket.service";
 import {LoggedInGuard} from "./shared/logged-in.guard";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import {TicketDetailscardComponent} from './ticket/ticket-detailscard/ticket-detailscard.component';
 import {BiddingCardComponent} from './ticket/bidding-card/bidding-card.component';
 import {MomentModule} from "angular2-moment";
 import 'moment/locale/hu';
 import {BidFormComponent} from './ticket/bid-form/bid-form.component';
-import {LoadingSpinnerComponent} from './core/loading-spinner/loading-spinner.component';
 import {BidService} from "./shared/bid.service";
 import * as firebase from 'firebase';
 import {environment} from "../environments/environment";
-import { NavBarItemComponent } from './core/nav-bar-item/nav-bar-item.component';
-import {EventModule} from "./event/event.module";
 import {EventcardModule} from "./event/eventcard/eventcard.module";
+import {CoreModule} from "./core/core.module";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    JumbotronComponent,
-
-    FooterComponent,
     ...AppRoutingModule.routableComponents,
     TicketDetailscardComponent,
     BiddingCardComponent,
     BidFormComponent,
-    LoadingSpinnerComponent,
-    NavBarItemComponent
   ],
   imports: [
     BrowserModule,
@@ -51,7 +40,8 @@ import {EventcardModule} from "./event/eventcard/eventcard.module";
     AppRoutingModule,
     HttpClientModule,
     MomentModule,
-    EventcardModule
+    EventcardModule,
+    CoreModule
   ],
   providers: [
     EventService,
