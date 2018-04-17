1084e4e50f47e6a39192bb18e6e1c0ff16ddb559
karon <kovacs.aron@dlg.hu>
2018-04-17 17:47:47 +0200

    [modify] = chat window handle scroll

--- src/app/chat/chat-window/chat-window.component.ts
+++ src/app/chat/chat-window/chat-window.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import "rxjs/add/operator/skip";
 import "rxjs/add/operator/distinctUntilChanged";
 import {environment} from "../../../environments/environment";
@@ -12,12 +12,21 @@ import {ChatService} from "../chat.service";
   templateUrl: './chat-window.component.html',
   styleUrls: ['./chat-window.component.css']
 })
-export class ChatWindowComponent implements OnInit {
+export class ChatWindowComponent implements OnInit, AfterViewChecked {
   @Input() roomId = environment.production ? null : MockedChatDatas.mockedRoomId;
   resetForm = false;
   chatMessage$: Observable<ChatMessageModel[]>;
+  @ViewChild('cardBody') cardBody: ElementRef;
+  private shouldScrolling = true;
 
   constructor(private chatService: ChatService) {
+  }
+
+  ngAfterViewChecked(): void {
+    if(this.shouldScrolling) {
+      this.cardBody.nativeElement.scrollTo(0, this.cardBody.nativeElement.scrollHeight);
+      this.shouldScrolling = false;
+    }
   }
 
   ngOnInit() {
@@ -29,6 +38,7 @@ export class ChatWindowComponent implements OnInit {
       .subscribe(
         resp => {
           if (resp) {
+            this.shouldScrolling = true;
             this.resetForm = true;
           } else {
             alert("Hiba az üzenetküldés közben");
